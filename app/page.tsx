8298d09dc81185a59a3e4ccce2058239b8dbfd78
Bruno Fonseca Souza <brunofsouza669@gmail.com>
2024-11-19 20:05:21 -0300

    Crud

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+import FormCadastroPlano from "@/components/FormCadastroPlano";
+import FormCartao from "@/components/FormCartao";
+import FormEndereco from "@/components/FormEndereco";
+import FormPerfil from "@/components/FormPerfil";
+import FormPlanoLoja from "@/components/FormPlanoLoja";
 import { Button } from "@/components/ui/button";
 import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-      <main className="flex space-x-2 items-center animate-pulse">
+      <main className="flex space-x-2 items-center">
         <ArrowLeftCircle className="w-12 h-12"/>
         <h1 className="font-bold">Crie um novo documento</h1>
-       
+        <FormPerfil></FormPerfil>
+        <FormEndereco></FormEndereco>
+        <FormCartao></FormCartao>
+
+        <FormPlanoLoja></FormPlanoLoja>
+        <FormCadastroPlano></FormCadastroPlano>
     </main>
   );
 }
