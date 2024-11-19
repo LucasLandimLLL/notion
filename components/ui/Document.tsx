5acdff5ebd6fbb093aabde39d993bc116ab38847
fonsecasBruno <brunofsouza669@gmail.com>
2024-11-19 00:02:56 -0300

    Criação do Bloco de Notas: Editor, Style, DarkMode

--- components/ui/Document.tsx
+++ components/ui/Document.tsx
@@ -6,6 +6,7 @@ import { Button } from "./button";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import Editor from "./Editor";
 
 function Document({ id }: { id: string }) {
     const[data, loading, error] = useDocumentData(doc(db, "documents", id))
@@ -51,7 +52,11 @@ function Document({ id }: { id: string }) {
 
             </div>
 
+            <hr className="pb-10"></hr>
+
             {/* collaborative editor*/}
+
+            <Editor />
         </div>
     );
 }
